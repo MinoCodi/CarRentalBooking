6d835af5a682ebd02451865e3b308c933a4a15ba
Vlad Shepshuk <vgoilik@gmail.com>
2018-03-16 18:37:29 +0300

    redux actions implementation

--- client/components/search/reducer.js
+++ client/components/search/reducer.js
@@ -1,4 +1,4 @@
-export default (state = { cars: [] }, action) => {
+/* export default (state = { cars: [] }, action) => {
   switch (action.type) {
     case "SET_SEARCH_PAGE":
       return { ...state, cars: action.payload.cars };
@@ -6,3 +6,42 @@ export default (state = { cars: [] }, action) => {
       return state;
   }
 };
+
+
+export function setSearchPage(url) {
+  return dispatch => {
+    dispatch({ type: "FETCH_REQUEST" });
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => dispatch({ type: "SET_SEARCH_PAGE", payload: data }))
+      .catch(error => dispatch({ type: "FETCH_ERROR", payload: error }));
+  };
+} */
+// ///////REDUX-ACTIONS/////////
+import { createActions, handleActions, combineActions } from "redux-actions";
+
+const defaultState = { cars: [] };
+
+const { fetch_request, set_search_page } = createActions({
+  FETCH_REQUEST: amount => ({ amount }),
+  SET_SEARCH_PAGE: url =>
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        data;
+      })
+});
+
+const reducer = handleActions(
+  {
+    [combineActions(fetch_request, set_search_page)](
+      state,
+      { payload: { payload } }
+    ) {
+      return { ...state, cars: payload };
+    }
+  },
+  defaultState
+);
+
+export default reducer;
